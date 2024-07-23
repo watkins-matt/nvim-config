c5c42b7b85bd8c2702ad3d7fdfeb96f37bd0f197
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-07-23 21:17:02 +0000

    Hide command line by default

--- lua/options.lua
+++ lua/options.lua
@@ -15,6 +15,9 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Hide command line when not in use
+vim.opt.cmdheight = 0
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
