17aacfb5b860377c179dc82ef9a21720cc22cd32
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-09-08 01:02:14 +0000

    Don't install ruff on termux

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,13 +8,14 @@ return {
   config = function()
     require('mason').setup()
 
-    local ensure_installed = { 'jq', 'jedi_language_server', 'ruff' }
+    local ensure_installed = { 'jq', 'jedi_language_server' }
 
     -- Ensure that stylua is installed on non-termux systems
     if not vim.g.is_termux then
       table.insert(ensure_installed, 'stylua')
       table.insert(ensure_installed, 'black')
       table.insert(ensure_installed, 'debugpy')
+      table.insert(ensure_installed, 'ruff')
     end
 
     require('mason-null-ls').setup {
