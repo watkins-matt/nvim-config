912a43c6f0b56a2da27709ed2795cac88e8b5dea
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-09-12 21:03:00 +0000

    Update debug config

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,90 +1,104 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		-- UI for nvim-dap
+		"rcarriga/nvim-dap-ui",
+		"rcarriga/nvim-notify",
+		-- Required for nvim-dap-ui
+		"nvim-neotest/nvim-nio",
 
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
+		-- Installs the debug adapters for you
+		"williamboman/mason.nvim",
+		"jay-babu/mason-nvim-dap.nvim",
 
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+		-- Python debugging
+		"mfussenegger/nvim-dap-python",
 
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-  },
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+		-- Dart debugging
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_setup = true,
+		-- Set nvim-notify as the default notification handler
+		vim.notify = require("notify")
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
+		-- Mason setup for automatic debug adapter installation
+		require("mason-nvim-dap").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"python",
+				"dart",
+			},
+		})
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        -- 'delve',
-      },
-    }
+		-- Custom dap-ui setup, only overriding specific fields
+		dapui.setup({
+			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" }, -- Custom icons
+			controls = { -- Custom control icons
+				element = "repl",
+				enabled = true,
+				icons = {
+					pause = "⏸",
+					play = "▶",
+					step_into = "⏎",
+					step_over = "⏭",
+					step_out = "⏮",
+					step_back = "b",
+					run_last = "▶▶",
+					terminate = "⏹",
+					disconnect = "⏏",
+				},
+			},
+			floating = {
+				border = "single",
+				mappings = {
+					close = { "q", "<Esc>" },
+				},
+			},
+		})
 
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
+		-- Automatically open and close dap-ui
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
+		-- Keybindings for debugging, all under <leader>d
+		vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Debug: Step Over" })
+		vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Debug: Step Out" })
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+		vim.keymap.set("n", "<leader>dB", function()
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end, { desc = "Debug: Set Conditional Breakpoint" })
+		vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Debug: Toggle REPL" })
+		vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Debug: Run Last" })
+		vim.keymap.set("n", "<leader>dx", dap.terminate, { desc = "Debug: Terminate" })
+		vim.keymap.set("n", "<leader>dt", dapui.toggle, { desc = "Debug: Toggle DAP UI" })
 
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+		-- Custom notification to avoid full-screen notifications
+		vim.notify = require("notify") -- Use nvim-notify instead of default vim.notify
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+		-- Check if debugpy is installed, otherwise print a warning
+		if vim.fn.executable("python") == 1 then
+			require("dap-python").setup("python")
+		else
+			vim.notify("Warning: Python executable not found! Python debugging won’t work.", vim.log.levels.WARN)
+		end
 
-    -- Install golang specific config
-    require('dap-go').setup()
-  end,
+		-- Check if Dart is installed before setting up
+		if vim.fn.executable("dart") == 1 then
+			require("dap-dart").setup()
+		else
+			--vim.notify("Warning: Dart executable not found! Dart debugging won’t work.", vim.log.levels.WARN)
+		end
+	end,
 }
