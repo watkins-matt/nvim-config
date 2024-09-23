eff88271837097a9e5b576f2873dd3a453a13fbf
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-09-23 20:12:41 +0000

    Fix issues finding venv

--- lua/utils/python_root.lua
+++ lua/utils/python_root.lua
@@ -3,19 +3,8 @@ local M = {}
 -- Hashtable to store directory -> project root mappings
 local project_root_cache = {}
 
--- Check if the current file is a Python file
-local function is_python_file()
-  local file_extension = vim.fn.expand '%:e'
-  return file_extension == 'py'
-end
-
 -- Function to find the Python project root based on markers
 function M.find_python_project_root()
-  -- Check if the file is a Python file
-  if not is_python_file() then
-    return nil -- If not a Python file, don't calculate the project root
-  end
-
   local cwd = vim.fn.expand '%:p:h'
 
   -- Check if the current directory is already cached
@@ -28,9 +17,10 @@ function M.find_python_project_root()
   local root = cwd
 
   -- Iterate upwards through parent directories to find the project root
-  while root do
+  while true do
     for _, marker in ipairs(root_markers) do
-      if vim.fn.isdirectory(root .. '/.venv') == 1 or vim.fn.filereadable(root .. '/' .. marker) == 1 then
+      local marker_path = root .. '/' .. marker
+      if vim.fn.isdirectory(marker_path) == 1 or vim.fn.filereadable(marker_path) == 1 then
         project_root_cache[cwd] = root -- Cache the project root for the current directory
         return root
       end
@@ -51,10 +41,9 @@ end
 -- Function to get the Python interpreter path (from .venv if available)
 function M.get_python_path()
   local project_root = M.find_python_project_root()
-
   if project_root then
     local venv_path = project_root .. '/.venv/bin/python'
-    if vim.fn.filereadable(venv_path) == 1 then
+    if vim.fn.executable(venv_path) == 1 then
       return venv_path
     end
   end
