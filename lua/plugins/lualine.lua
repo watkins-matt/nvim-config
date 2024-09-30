a84c16d44b395141314055b875f79ed1744e8312
Matt Watkins <48530805+watkins-matt@users.noreply.github.com>
2024-09-30 21:37:29 +0000

    Ignore debug ui, neotest and outline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,32 +1,42 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", lazy = false },
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				theme = "palenight",
-				section_separators = { "", "" },
-				component_separators = { "", "" },
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "hostname", "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			extensions = { "nvim-tree" },
-		})
-	end,
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons', lazy = false },
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        theme = 'palenight',
+        section_separators = { '', '' },
+        component_separators = { '', '' },
+        ignore_focus = {
+          'dapui_breakpoints',
+          'dapui_scopes',
+          'dapui_stacks',
+          'dapui_watches',
+          'dapui_console',
+          'dap-repl',
+          'neotest-summary',
+          'Outline',
+        },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'hostname', 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = { 'nvim-tree' },
+    }
+  end,
 }
